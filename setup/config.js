fd8033ae7409bbc7af9adad28bdc4bf070e472b2
Aditya <53331534+addy123d@users.noreply.github.com>
2021-05-13 21:32:02 +0530

    Update config.js

--- setup/config.js
+++ setup/config.js
@@ -1,5 +1,5 @@
 const configObj = {
-    url: "mongodb+srv://vaishnavi:FG8sduFhTeW1OC13@cluster0.aed7e.mongodb.net/project0?retryWrites=true&w=majority"
+    url: "mongodb+srv://<username>:<password>@cluster0.aed7e.mongodb.net/project0?retryWrites=true&w=majority"
 }
 
 module.exports = configObj;
