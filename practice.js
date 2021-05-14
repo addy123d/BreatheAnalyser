1fac1a69cc1692d15e729bb3c0cdfd7f1f03ae82
unknown <acd3101282@gmail.com>
2021-05-14 18:33:58 +0530

    v3.0

--- practice.js
+++ practice.js
@@ -1,3 +1,5 @@
-const date = new Date().toString();
+// const readingTime = Date.now("Fri May 14 2021 12:15:40 GMT+0530 (India Standard Time)");
+// const live = Date.now();
 
-console.log(date);
+// console.log(readingTime);
+// console.log(live);
